a0bc21b561c7b85b53cfe09156a0c8cb30775a6c
Julien <julien.morel76350@gmail.com>
2024-02-14 16:48:07 +0100

    💄Update header css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,63 +19,14 @@ import {
 import { PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid';
 import { supabase } from '../../src/app/components/Server/supabase';
 
-const products = [
-  { name: 'Analytics', description: 'Get Link better understanding of your traffic', href: '#', icon: ChartPieIcon },
-  { name: 'Engagement', description: 'Speak directly to your customers', href: '#', icon: CursorArrowRaysIcon },
-  { name: 'Security', description: 'Your customers’ data will be safe and secure', href: '#', icon: FingerPrintIcon },
-  { name: 'Integrations', description: 'Connect with third-party tools', href: '#', icon: SquaresPlusIcon },
-  { name: 'Automations', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
-]
-const callsToAction = [
-  { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
-  { name: 'Contact sales', href: '#', icon: PhoneIcon },
-]
-
 const navigation = {
   company: [
     { name: 'Faq', href: '#' },
   ],
   legal: [
     { name: 'Content privacy', href: '#' },
-    { name: 'Privacy', href: '#' },
-    { name: 'Terms', href: '#' },
   ],
   social: [
-    {
-      name: 'Facebook',
-      href: 'https://www.facebook.com/',
-      icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
-        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-          <path
-            fillRule="evenodd"
-            d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
-            clipRule="evenodd"
-          />
-        </svg>
-      ),
-    },
-    {
-      name: 'Instagram',
-      href: 'https://www.instagram.com/',
-      icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
-        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-          <path
-            fillRule="evenodd"
-            d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
-            clipRule="evenodd"
-          />
-        </svg>
-      ),
-    },
-    {
-      name: 'Twitter',
-      href: 'https://twitter.com/',
-      icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
-        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-          <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
-        </svg>
-      ),
-    },
     {
       name: 'GitHub',
       href: 'https://github.com/seinokoula/open_new_teck_a4',
@@ -136,7 +87,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <ThemeProvider enableSystem={true} attribute="class">
           <div>
             <header className="">
-              <nav className="mx-auto flex max-w-7xl items-center justify-between px-6 py-12 lg:px-16" aria-label="Global">
+              <nav className="mx-auto flex max-w-7xl items-center justify-between relative  px-6 py-12 lg:px-16" aria-label="Global">
                 <div className="flex">
                   <Link href="/" className="-m-1.5 p-1.5">
                     <span className="sr-only">Open Tech</span>
@@ -153,16 +104,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                     <Bars3Icon className="h-6 w-6" aria-hidden="true" />
                   </button>
                 </div>
-                <div className="flex flex-auto justify-center">
-                  <Popover.Group className="max-sm:invisible flex gap-x-12">
+                <div className=" absolute hidden lg:flex top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                  <Popover.Group className="max-sm:invisible flex gap-x-6 rounded-full bg-[rgba(39,39,42,.9)] px-4 py-2 border-2 border-secondary_200">
                     <div>
-                      <Link href="profile" className="text-sm font-semibold leading-6">Profile</Link>
+                      <Link href="profile" className="text-sm font-medium leading-6 py-2  hover:text-primary_300 transition">Profile</Link>
                     </div>
                     <div>
-                      <Link href="faq" className="text-sm font-semibold leading-6">Faq</Link>
-                    </div>
-                    <div>
-                      <Link href="roadmap" className="text-sm font-semibold leading-6">Roadmap</Link>
+                      <Link href="faq" className="text-sm font-medium leading-6 py-2  hover:text-primary_300 transition">Faq</Link>
                     </div>
                   </Popover.Group>
                 </div>
@@ -184,7 +132,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                   <p className='py-2'>{email}</p>
                 </div>
               </nav>
-
               <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
                 <div className="fixed inset-0 z-10" />
                 <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-black px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
