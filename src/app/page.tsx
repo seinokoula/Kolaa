8200128af8c0deadd1454f98605d2bd3ff1265d3
Julien <julien.morel76350@gmail.com>
2024-02-15 10:31:07 +0100

    ✅ update Readme header And addprivacy page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,17 @@
 'use client';
 
-import { ArrowPathIcon, CloudArrowUpIcon, FingerPrintIcon, LockClosedIcon } from '@heroicons/react/24/outline'
-import { motion } from "framer-motion"
 import SearchBar from '../app/components/SearchBar'
 import GetAllPost from '../app/components/GetAllPost'
 import Header from './components/Header';
 import { useState } from 'react';
 
-const features = [
-  {
-    name: 'Push to deploy',
-    description:
-      'Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi. Odio urna massa nunc massa.',
-    icon: CloudArrowUpIcon,
-  },
-  {
-    name: 'SSL certificates',
-    description:
-      'Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget. Sem sodales gravida quam turpis enim lacus amet.',
-    icon: LockClosedIcon,
-  },
-  {
-    name: 'Simple queues',
-    description:
-      'Quisque est vel vulputate cursus. Risus proin diam nunc commodo. Lobortis auctor congue commodo diam neque.',
-    icon: ArrowPathIcon,
-  },
-  {
-    name: 'Advanced security',
-    description:
-      'Arcu egestas dolor vel iaculis in ipsum mauris. Tincidunt mattis aliquet hac quis. Id hac maecenas ac donec pharetra eget.',
-    icon: FingerPrintIcon,
-  },
-]
 
 export default function Example() {
   const [searchTermForPosts, setSearchTermForPosts] = useState<string>('');
 
   return (
     <div className="mw">
+      <link rel="icon" href="/src/app/favicon.ico" sizes="any" />
       <Header />
       <SearchBar setSearchTermForPosts={setSearchTermForPosts} />
       <GetAllPost searchTerm={searchTermForPosts} />
